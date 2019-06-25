b6cf1fec7498f21b8339020133175e114a6c8898
Abhinav Bansal <coolvasu.bansal@gmail.com>
2019-06-25 19:23:46 +0530

    updated init.js

--- js/init.js
+++ js/init.js
@@ -9,7 +9,7 @@ window.addEventListener('DOMContentLoaded', function() {
     var video = document.getElementById(cam_video_id);
     var options = {
         "audio": false,
-        "video": true
+        "video": { facingMode: "environment"  }
     };
     // Replace the source of the video element with the stream from the camera
     if (navigator.getUserMedia) {
